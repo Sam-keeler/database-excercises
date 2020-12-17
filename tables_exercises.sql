89593070be1e19a56fd3105ea11bfe19493393ba
Sam Keeler <keelersam@gmail.com>
2020-12-16 22:41:49 -0600

    Adding my first sql query

---
+++ tables_exercises.sql
@@ -0,0 +1,11 @@
+USE employees;
+SHOW TABLES;
+DESCRIBE employees_with_departments;
+DESCRIBE dept_manager;
+#employeees contains numeric, string, and dates
+#Numeric:
+ #dept_emp, dept_manager, employees, 
+#String:
+ #departments, dep_emp, dept_manager, employees, 
+#Date:
+ #dep_emp, dept_manager, employees, 
