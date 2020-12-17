00adc9b31d41e8fb742cecb9b47a0100592c4e1a
Sam Keeler <keelersam@gmail.com>
2020-12-16 22:49:44 -0600

    Completing the exercise

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,11 +1,11 @@
 USE employees;
 SHOW TABLES;
 DESCRIBE employees_with_departments;
-DESCRIBE dept_manager;
+DESCRIBE titles;
 #employeees contains numeric, string, and dates
 #Numeric:
- #dept_emp, dept_manager, employees, 
+ #dept_emp, dept_manager, employees, employees_with_departments, salaries, titles
 #String:
- #departments, dep_emp, dept_manager, employees, 
+ #departments, dep_emp, dept_manager, employees, employees_with_departments, titles
 #Date:
- #dep_emp, dept_manager, employees, 
+ #dep_emp, dept_manager, employees, salaries, titles
