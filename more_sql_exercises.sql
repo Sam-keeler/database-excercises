4598bb9a62747b3fb95eb1f03f03398ac6c27664
Sam Keeler <keelersam@gmail.com>
2021-01-07 11:48:47 -0600

    Continuing more_exercises

--- more_sql_exercises.sql
+++ more_sql_exercises.sql
@@ -2,19 +2,16 @@
 # relative to the average salary for the department?
 # Is there any department where the department manager gets
 # paid less than the average salary?
-SELECT salary, CONCAT(first_name, ' ', last_name) AS 'Manager Name', dept_name
+SELECT ROUND(AVG(salaries.salary), 0) AS 'Average Salary', dept_name, msalaries.salary AS 'Manager Salary'
 FROM salaries
+JOIN employees USING(emp_no)
 JOIN employees_with_departments USING(emp_no)
-WHERE salaries.emp_no IN (SELECT emp_no
-                 FROM dept_manager
-                 WHERE to_date > CURDATE())            
-AND to_date > CURDATE()
-UNION
-SELECT dept_name, AVG(salary)
-FROM salaries
-JOIN employees_with_departments USING(emp_no)
-WHERE to_date > CURDATE()
-
+JOIN dept_manager ON employees_with_departments.emp_no = dept_manager.emp_no
+   AND dept_manager.to_date > CURDATE()
+JOIN employees AS managers ON managers.emp_no = dept_manager.emp_no
+JOIN salaries AS msalaries ON managers.emp_no = msalaries.emp_no
+   AND msalaries.to_date > CURDATE()
+GROUP BY dept_name, msalaries.salary
 
 
 
