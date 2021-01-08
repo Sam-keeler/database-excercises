984be637cd7a993af6eb3f942aa5341f80351ab6
Sam Keeler <keelersam@gmail.com>
2021-01-08 10:20:41 -0600

    Adding bonus onto case exercise

--- case_exercises.sql
+++ case_exercises.sql
@@ -44,4 +44,64 @@ FROM Decades;
 
 SELECT COUNT(decade_group) AS 'Employee count', decade_group
 FROM Decades
-GROUP BY decade_group
+GROUP BY decade_group;
+
+# What is the current average salary for each of the following department groups:
+# R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
+CREATE TEMPORARY TABLE d_averages(
+customer_service INT);
+
+INSERT INTO d_averages(customer_service)
+SELECT salary
+FROM employees.salaries
+JOIN employees.dept_emp USING(emp_no)
+WHERE dept_no = 'd009'
+AND salaries.to_date > CURDATE();
+
+ALTER TABLE d_averages ADD finance_hr INT;
+
+INSERT INTO d_averages(finance_hr)
+SELECT salary
+FROM employees.salaries
+JOIN employees.dept_emp USING(emp_no)
+WHERE dept_no = 'd003'
+OR dept_no = 'd002'
+AND salaries.to_date > CURDATE();
+
+ALTER TABLE d_averages ADD sales_marketing INT;
+
+INSERT INTO d_averages(sales_marketing)
+SELECT salary
+FROM employees.salaries
+JOIN employees.dept_emp USING(emp_no)
+WHERE dept_no = 'd001'
+OR dept_no = 'd007'
+AND salaries.to_date > CURDATE();
+
+ALTER TABLE d_averages ADD prod_qm INT;
+
+INSERT INTO d_averages(prod_qm)
+SELECT salary
+FROM employees.salaries
+JOIN employees.dept_emp USING(emp_no)
+WHERE dept_no = 'd004'
+OR dept_no = 'd006'
+AND salaries.to_date > CURDATE();
+
+ALTER TABLE d_averages ADD r_d INT;
+
+INSERT INTO d_averages(r_d)
+SELECT salary
+FROM employees.salaries
+JOIN employees.dept_emp USING(emp_no)
+WHERE dept_no = 'd005'
+OR dept_no = 'd008'
+AND salaries.to_date > CURDATE();
+
+SELECT AVG(customer_service) AS 'CS Average', AVG(finance_hr) AS 'Finance and HR Average', 
+AVG(sales_marketing) AS 'Sales and Mark Average', AVG(prod_qm) AS 'Prod and QM Average', 
+AVG(r_d) AS 'R & D Average'
+FROM d_averages;
+
+SELECT *
+FROM d_averages
